1d90cf55803b4c438547ad544605a09da46637b9
dieovitski <ihsan.furkan@gmail.com>
2021-09-23 17:44:27 +0300

    added scss

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className='homepage' >
+      <div className='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+          <h1 className='title'>Sapkalar</h1>
+          <span className='subtitle'>Alisveris yap</span>
+          </div> 
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+          <h1 className='title'>Montlar</h1>
+          <span className='subtitle'>Alisveris yap</span>
+          </div> 
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+          <h1 className='title'>Ayakkabilar</h1>
+          <span className='subtitle'>Alisveris yap</span>
+          </div> 
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+          <h1 className='title'>Kadin</h1>
+          <span className='subtitle'>Alisveris yap</span>
+          </div> 
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+          <h1 className='title'>Erkek</h1>
+          <span className='subtitle'>Alisveris yap</span>
+          </div> 
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
